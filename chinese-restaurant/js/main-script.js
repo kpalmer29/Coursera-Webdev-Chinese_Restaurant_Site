8b9ab7846ab0ec53af934dee8dd970dcbee57169
Kavi Palmer <kavipalmer@Kavis-MacBook-Pro.local>
2022-07-15 13:03:00 -0500

    Commented

--- chinese-restaurant/js/main-script.js
+++ chinese-restaurant/js/main-script.js
@@ -13,33 +13,44 @@
 
     var dc = {};
 
+    //html for jumbotron
     var homeHtml = "snippets/home-snippet.html";
+
+    //URL to receive Menu-Categories JSON
     var categoriesURL ="https://davids-restaurant.herokuapp.com/categories.json";
 
+    //HTML for building menu-categories page
     var categoriesTitleHTML = "snippets/menu-categories.html";
     var categoryHTML = "snippets/category-snippet.html";
 
+    //URL to receive JSON for a single menu-category page
     var menuItemsURL = "https://davids-restaurant.herokuapp.com/menu_items.json?category=";
+
+    //HTML for building single menu-category page
     var menuItemsTitle = "snippets/menu-items-title.html";
     var menuItemHTML = "snippets/menu-item.html";
 
+    //helper function for inserting html into a target element id.
     var insertHtml = function(selector,html) {
         var targetElem = document.querySelector(selector);
         targetElem.innerHtml = html;
     };
 
+    //helper function to display Ajax-loader while content is being loaded
     var showloading = function(selector) {
         var html = "<div class='text-center'>";
         html += '<img src="https://commons.wikimedia.org/wiki/File:Ajax-loader.gif"></div>';
         insertHtml(selector,html);
     };
 
+    //helper function to replace placeholders in HTML
     var insertProperty = function (string, propName, propValue) {
         var propToReplace = "{{" + propName + "}}";
         string = string.replace(new RegExp(propToReplace, "g"), propValue);
         return string;
     }
 
+    //function to load the jumbotron into #main-content upon page loading
     document.addEventListener("DOMContentLoaded", function (event){
 
         showloading("#main-content");
@@ -51,18 +62,23 @@
             false);
     });
 
+    //function to load the menu-categories page into #main-content
     dc.loadMenuCategories = function () {
         showloading("#main-content");
         $ajaxUtils.sendGetRequest(categoriesURL, buildAndShowCategoriesHTML, false);
     };
 
+    //function to load the menu-items (single category) page into #main-content
     dc.loadMenuItems = function (categoryShort) {
-        showloading("#main-content");
+        showloading("#mgitain-content");
         $ajaxUtils.sendGetRequest(
             menuItemsURL + categoryShort,
             buildAndShowMenuItemsHTML);
     };
 
+
+    //loadMenuItems helper. Calls buildMenuItemsViewHTML and insertHtml to generate
+    //and insert the html needed to display the menu-items page
     function buildAndShowMenuItemsHTML (categoryMenuItems) {
 
         $ajaxUtils.sendGetRequest(
@@ -83,6 +99,8 @@
         false);
     }
 
+    //buildAndShowMenuItems helper functions. Generates the HTML which the calling function inserts
+    //into #main-section
     function buildMenuItemsViewHtml (categoryMenuItems,
                                      menuItemsTitleHTML,
                                      menuItemHtml) {
@@ -121,6 +139,8 @@
         return finalHtml;
     }
 
+    //buildMenuItemsViewHtml helper function. Utilizes insertProperty to replace price placeholders in
+    //menu-item.html
     function insertItemPrice(html, name, priceValue) {
 
         if (!priceValue) {
@@ -132,6 +152,8 @@
         return html;
     }
 
+    //buildMenuItemsViewHtml helper function. Utilizes insertProperty to replace portion placeholders in
+    //menu-item.html
     function insertItemPortionName(html, name, portionValue) {
 
         if(!portionValue) {
@@ -141,6 +163,7 @@
 
     }
 
+    //loadMenuCategories helper function. Generates and inserts code to display menu-categories page
     function buildAndShowCategoriesHTML (categories) {
         $ajaxUtils.sendGetRequest(
             categoriesTitleHTML,
@@ -159,6 +182,7 @@
             false);
     }
 
+    //buildAndShowCategoriesHTML helper function. Generates the HTML which is then inserted into #main-content
     function buildCategoriesViewHtml (categories,
                                       categoriesTitleHTML,
                                       categoryHTML) {
